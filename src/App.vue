7b4caefd6405fa047a833f6d2cd3fe2132b2327e
Casmir Onyeka <casmir293@gmail.com>
2024-01-18 16:40:42 +0100

    practice 2

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,28 @@
 <template>
-  <div></div>
+  <div>
+    <h1>HELLO, WORLD!</h1>
+    <p>Hello {{ name }} - {{ age }}</p>
+    <button @click="changeName('Ben')">Change Name</button>
+    <button @click="changeAge(49)">Change Name</button>
+
+    <h2>{{ jobs[0].location }}</h2>
+  </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { ref } from 'vue'
+import jobs from '../src/types/Job'
+
+const name = ref<string>('Casmir')
+const age = ref<number>(26)
+
+const changeName = (newName: string) => {
+  name.value = newName
+}
+
+const changeAge = (newAge: number) => {
+  age.value = newAge
+}
+</script>
 
 <style scoped></style>
